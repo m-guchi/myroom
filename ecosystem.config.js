a1e69ece8481678d0b80f24a270f2abc2d2ed618
guchi main-WSL <kazu9sum.mi@gmail.com>
2026-02-19 02:59:38 +0900

    Swagger UI 修正

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "insight-backend",
         script: "./venv/bin/uvicorn",
-        args: "backend.main:app --host 127.0.0.1 --port 8000 --root-path /insight-myroom/api",
+        args: "backend.main:app --host 127.0.0.1 --port 8000",
         interpreter: "none",
         cwd: "./",
         watch: false,
